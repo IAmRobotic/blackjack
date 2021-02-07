5860cf086343de6566a47ef8fc878410a55f2da4
Alex <iamroboticwiz@gmail.com>
2021-02-07 18:04:28 -0500

    Added main gameplay loop.  Still missing splitting Aces and betting functionality.

--- blackjack.py
+++ blackjack.py
@@ -58,6 +58,7 @@ class Hand:
         self.player_type = player_type.lower()
         self._cards = ()  # empty tuple
         self._score = 0
+        self._initial_deal = True  # only used if player_type is 'dealer'
 
         if self.player_type not in self.valid_player_types:
             raise Exception("Player type must be 'player', 'dealer' or 'computer'")
@@ -71,14 +72,21 @@ class Hand:
         player_type = self.player_type.capitalize()
         cards = ""
         msg = ""
-        for card in self._cards:
-            cards += card[0] + SUIT_SYMBOLS[card[1]] + " "
-        if self._score > 21:
-            msg = "***BUST!***"
-        elif self._score == 21:
-            msg = "***BLACKJACK!!***"
 
-        return f"{player_type}: {cards}\nScore: {self._score} {msg}"
+        if (self.player_type == 'dealer') and self._initial_deal and (len(self._cards) == 2):
+            cards += self._cards[0][0] + SUIT_SYMBOLS[self._cards[0][1]]
+            cards += " \U0001F0A0"  # second card for dealer shown face down until player finishes
+            return f"{player_type}: {cards}"
+
+        else:
+            for card in self._cards:
+                cards += card[0] + SUIT_SYMBOLS[card[1]] + " "
+            if self._score > 21:
+                msg = "***BUST!***"
+            elif self._score == 21:
+                msg = "***BLACKJACK!!***"
+
+            return f"{player_type}: {cards} | Score: {self._score} {msg}"
 
     def calculate_score(self):
 
@@ -114,5 +122,51 @@ def initial_deal(deck_object, dealer, *players):
         dealer.draw(deck_object)
 
 
-def dealer_move(dealer_hand):
-    pass
+def main(num_decks = 6):
+    valid_inputs = ('h', 's')
+    deck = CommunityCards(num_decks)
+    player = Hand()
+    dealer = Hand('dealer')
+    initial_deal(deck, dealer, player)
+
+    print(dealer)
+    print(player, '\n')
+
+    if player._score < 21: # player's turn
+        active_turn = True
+        while player._score < 21 and active_turn:
+            invalid_input = True
+            while invalid_input:
+                hit_or_stand = input("Do you want to H)it or S)tand? -> ").lower()
+                if hit_or_stand in valid_inputs:
+                    invalid_input = False
+                else:
+                    print("Please enter 'H' for another card or 'S' to end turn")
+
+            if hit_or_stand == 'h':
+                player.draw(deck, 1)
+                print(player, '\n')
+            else:
+                active_turn = False
+
+    if player._score < 22: # dealer's turn
+        dealer._initial_deal = False
+        print('\n',dealer)
+        while dealer._score < 17:
+            print("***Dealer draws a card***")
+            dealer.draw(deck, 1)
+            print(dealer, '\n')
+
+    dealer._initial_deal = False
+    print("\nFinal hands:")
+    print(dealer)
+    print(player, '\n')
+
+    if (22 > player._score > dealer._score) or ((dealer._score > 21) and (player._score < 22)):
+        print("!!!You win!!!")
+    elif (22 > dealer._score > player._score) or ((player._score > 21) and (dealer._score < 22)):
+        print("Dealer wins, you lose :(")
+    else:
+        print("Tie")
+
+main()
